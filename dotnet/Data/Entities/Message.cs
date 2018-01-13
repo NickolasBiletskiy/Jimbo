160b6cfbfcaba243d142aad5b92aa89859a35fba
DESKTOP-QJG1QI6\Nickolas <supernickola@ukr.net>
2018-01-13 15:49:45 +0200

    added db conenection classes. Not working

---
+++ dotnet/Data/Entities/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities
+{
+    public class Message : BaseEntity
+    {
+        [Key]
+        public Guid MessageId { get; set; }
+        public Guid UserFromId { get; set; }
+        public Guid UserToId { get; set; }
+        public string MessageText { get; set; }
+        public DateTime SentTS { get; set; }
+        public DateTime LastSyncBySenderTS { get; set; }
+        public DateTime LastSyncByReceiverTS { get; set; }
+
+        public virtual User UserFrom { get; set; }
+        public virtual User UserTo { get; set; }
+    }
+}
